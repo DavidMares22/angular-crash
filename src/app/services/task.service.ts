55e821a767b712c36dafe05164b0d9c7df5e5fb0
damr <damr18@gmail.com>
2021-06-19 12:09:38 -0500

    httpclient for service

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task } from '../Task';
 import { TASKS } from '../test-tasks';
 
@@ -7,10 +8,12 @@ import { TASKS } from '../test-tasks';
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+
+  private apiUrl  = 'http://localhost:5000/tasks'
+
+  constructor(private http:HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS)
-    return tasks;
+     return this.http.get<Task[]>(this.apiUrl)
   }
 }
